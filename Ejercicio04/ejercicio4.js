53383cc36b6c9d0ed21f4638ddaa9e87a8e52082
Johan Polo <johan.s310806@gmail.com>
2025-03-04 20:02:35 -0500

    Ejercicio 4

---
+++ Ejercicio04/ejercicio4.js
@@ -0,0 +1,19 @@
+function filter(Array, callback) {
+    let newArray = [];
+    for(let i = 0; i < array.length; i++){
+        if(callback(Array[i]) == true){
+            newArray.push(Array[i]);
+        }
+    }
+    return newArray;
+}
+
+let validarNum = num => {
+    if (typeof num === "number") {
+        return  true;
+    }
+    return false;
+}
+
+let array = [1, 2, "tres", "4", 5];
+console.log(filter(array, validarNum));
