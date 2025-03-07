0d84a726987f7ff37ad4a49802cb84183a4d5d9c
Johan Polo <johan.s310806@gmail.com>
2025-03-07 17:39:51 -0500

    Comantarios Ejercicio 3

--- Ejercicio03/ejercicio3.js
+++ Ejercicio03/ejercicio3.js
@@ -1,12 +1,19 @@
+// Creo función donde se recibe como parametro un arreglo y una callback
 function map(array, callback) {
+    // Declaro e asigno a una variable un arreglo vacio.
     let newArray = [];
+    // Por medio de un bucle for recorro cada elemento del arreglo recibido y lo agrego al arreglo creado
     for (let i = 0; i < array.length; i++) {
         newArray.push(callback(array[i]));
     }
+    // Por ultimo retorno el arreglo creado dentro de la función.
     return newArray;
 }
 
+// Decfalro e asigno a una varaible un array.
 let array = [1, 2, 3, 4, 5];
+// Declaro varible donde se llama a la funcion map enviado como argumento el arreglo y una arrow funtion (callback) que multiplica cada elemento por 2.
 let duplicado = map(array, num => num * 2);
 
+// Se imprime por pantalla el valor almacenado en la variable diplicado que lo que almacena es el valor retornado por la función que se llama.
 console.log(duplicado);
